36af73254efadda34e013889baf5563b899b0c5a
Steve Rubin <nerdforlife@gmail.com>
2014-03-02 14:07:30 -0800

    pitch tracker

---
+++ PitchTracker/src/com/example/pitchtracker/AudioProc.java
@@ -0,0 +1,75 @@
+package com.example.pitchtracker;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import be.hogent.tarsos.dsp.AudioEvent;
+
+public class AudioProc {
+	
+	public interface OnAudioEventListener {
+		public void processAudioProcEvent(AudioEvent ae);
+	}
+
+	private int mBufferSize;
+	private short[] mBuffer;
+	private AudioRecord mRecorder;
+	private int mSampleRate;
+	private boolean mIsRecording = false;
+	private be.hogent.tarsos.dsp.AudioFormat mTarsosFormat;
+	private AudioProc.OnAudioEventListener mOnAudioEventListener;
+
+	public AudioProc(int sampleRate) {
+		mSampleRate = sampleRate;
+		mBufferSize = AudioRecord.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_IN_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
+		mBuffer = new short[mBufferSize];
+		mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+				mSampleRate,
+				AudioFormat.CHANNEL_IN_MONO,
+				AudioFormat.ENCODING_PCM_16BIT,
+				mBufferSize);
+		mTarsosFormat = new be.hogent.tarsos.dsp.AudioFormat(
+				(float)mSampleRate, 16, 1, true, true);
+	}
+	
+	public int getBufferSize() {
+		return mBufferSize;
+	}
+	
+	public boolean isRecording() {
+		return mIsRecording;
+	}
+	
+	public void listen() {
+		mRecorder.startRecording();
+		mIsRecording  = true;
+		processAudio();
+	}
+	
+	public void stop() {
+		mIsRecording = false;
+		mRecorder.stop();
+	}
+	
+	public void setOnAudioEventListener(AudioProc.OnAudioEventListener listener) {
+		mOnAudioEventListener = listener;
+	}
+	
+	private void processAudio() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while (mIsRecording) {
+					int bufferReadResult = mRecorder.read(mBuffer, 0, mBufferSize);
+					AudioEvent audioEvent = new AudioEvent(mTarsosFormat, bufferReadResult);
+					audioEvent.setFloatBufferWithShortBuffer(mBuffer);
+					if (mOnAudioEventListener != null) {
+						mOnAudioEventListener.processAudioProcEvent(audioEvent);	
+					}
+				}
+			}
+		});
+	}
+}
---
+++ PitchTracker/src/com/example/pitchtracker/PitchTrackerActivity.java
@@ -0,0 +1,89 @@
+package com.example.pitchtracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import be.hogent.tarsos.dsp.AudioEvent;
+import be.hogent.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.hogent.tarsos.dsp.pitch.PitchDetectionResult;
+import be.hogent.tarsos.dsp.pitch.PitchProcessor;
+
+public class PitchTrackerActivity extends Activity implements AudioProc.OnAudioEventListener, PitchDetectionHandler {
+
+	private static final int SAMPLE_RATE = 16000;
+	private AudioProc mAudioProc;
+	private PitchProcessor mPitchProcessor;
+	private TextView mPitchBox;
+	private Button mListenButton;
+	private static final String TAG = "PitchTrackerActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pitch_tracker);
+		
+		mAudioProc = new AudioProc(SAMPLE_RATE);
+		setPitchProcessor();
+		mAudioProc.setOnAudioEventListener(this);
+		mPitchBox = (TextView)findViewById(R.id.pitch);
+		mListenButton = (Button)findViewById(R.id.listen);
+		
+		mListenButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (mAudioProc.isRecording()) {
+					mAudioProc.stop();
+					mListenButton.setText("Listen");
+				} else {
+					mAudioProc.listen();
+					mListenButton.setText("Stop listening");
+				}
+			}
+		});
+		
+	}
+
+	public void setPitchProcessor() {
+		PitchProcessor.PitchEstimationAlgorithm alg = 
+				PitchProcessor.PitchEstimationAlgorithm.AMDF;
+		mPitchProcessor = new PitchProcessor(alg, SAMPLE_RATE, mAudioProc.getBufferSize(), this);
+	}
+		
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.pitch_tracker, menu);
+		return true;
+	}
+
+	@Override
+	public void processAudioProcEvent(AudioEvent ae) {
+		// detect pitch
+		mPitchProcessor.process(ae);
+		Log.d(TAG, "Processing pitch");
+	}
+
+	@Override
+	public void handlePitch(final PitchDetectionResult pitchDetectionResult,
+			AudioEvent audioEvent) {
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				if (pitchDetectionResult.isPitched()) {
+					mPitchBox.setText(String.valueOf(pitchDetectionResult.getPitch()) + " Hz");
+				} else {
+					mPitchBox.setText("No pitch detected");
+				}
+			
+			}
+			
+		});
+	}
+
+}
